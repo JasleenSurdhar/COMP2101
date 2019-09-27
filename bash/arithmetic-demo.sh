876b3e21e2f66b4186c4e1923c9bcead7872531b
JasleenSurdhar <lolasurdhar@gmail.com>
2019-09-27 05:53:59 -0700

    arithmatic

--- bash/arithmetic-demo.sh
+++ bash/arithmetic-demo.sh
@@ -8,7 +8,7 @@
 #   - the output should look something like:
 #   - first divided by second gives X with a remainder of Y
 # Improve this script by calculating and displaying the first number raised to the power of the second number
-export remainder 
+export remainder
 read -p "Input1? " firstnumber
 read -p "Input2? " secondnumber
 sum=$((firstnumber + secondnumber))
@@ -22,6 +22,7 @@ fpdividend=$(awk "BEGIN{printf \"%.2f\", $firstnumber/$secondnumber}")
 cat <<EOF
 $firstnumber plus $secondnumber is $sum
 $firstnumber divided by $secondnumber is $dividend
+$firstnumber modulas with $secondnumber gives $remainder
   - More precisely, it is $fpdividend
 $firstnumber multiplied by $secondnumber is $multiply
 $firstnumber subtractng by $secondnumber is $sub
